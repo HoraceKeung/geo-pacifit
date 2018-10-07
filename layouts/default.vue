a0f01a310382ae9b263b8b76fd9992805bc5fd70
Horace Keung <horacekeung@hotmail.com>
2018-10-07 15:34:11 +0100

    edge style fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,10 @@
 		<div class="flex-1"><nuxt/></div>
 		<back-to-top>
 			<button class="btn w-12 h-12 flex" @mouseover="hover = true" @mouseleave="hover = false">
-				<img v-show="!hover" class="w-4 m-auto" src="~/assets/img/back-to-top.png">
-				<img v-show="hover" class="w-4 m-auto" src="~/assets/img/back-to-top-white.png">
+				<div class="m-auto">
+					<img v-show="!hover" class="w-4" src="~/assets/img/back-to-top.png">
+					<img v-show="hover" class="w-4" src="~/assets/img/back-to-top-white.png">
+				</div>
 			</button>
 		</back-to-top>
 	</div>
