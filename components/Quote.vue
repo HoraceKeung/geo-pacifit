46f04ef81c4d96edd1906b58d0b4f154e6d522c9
Horace Keung <horacekeung@hotmail.com>
2018-10-14 12:22:47 +0100

    no yarn

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="relative"><div class="absolute w-full flex" style="bottom: -2rem;">
-        <div class="rounded-full shadow bg-white h-16 w-16 flex mx-auto">
-            <div class="m-auto"><img class="w-8" src="~assets/img/quote.png"></div>
+    <div class="relative"><div class="absolute w-full flex" style="bottom: -2.5rem;">
+        <div class="rounded-full shadow bg-white h-20 w-20 flex mx-auto">
+            <div class="m-auto"><img class="w-10" src="~assets/img/quote.png"></div>
         </div>
     </div></div>
 </template>
